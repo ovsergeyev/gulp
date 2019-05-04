b7794d62e4e782e703bdef30c4be4157004fee7b
Sergeev Oleg <o.v.sergeyev@gmail.com>
2019-05-04 13:07:47 +0300

    Сверстана первая страница

--- gulp/config/tasks.js
+++ gulp/config/tasks.js
@@ -2,8 +2,8 @@ module.exports = [
  './gulp/tasks/pug',
  './gulp/tasks/html-lint',
  './gulp/tasks/serve',
- './gulp/tasks/stylus',
- './gulp/tasks/sass',
+ //'./gulp/tasks/stylus',
+ //'./gulp/tasks/sass',
  './gulp/tasks/less',
  './gulp/tasks/watch'
 ];
--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -1,15 +1,18 @@
 module.exports = function () {
     $.gulp.task('less', function () {
         return $.gulp.src('src/less/main.less')
+            .pipe($.plumber({
+                    errorHandler: $.gp.notify.onError()
+            }))
             //.pipe($.gp.sourcemaps.init())
             .pipe($.gp.less({}))
             .pipe($.gp.autoprefixer({
-                browsers: ['last 10 versions'],
+                browsers: ['last 1 versions'],
                 cascade: false
             }))
-            .on("error", $.gp.notify.onError({
-                titile: "stile"
-            }))
+            //.on("error", $.gp.notify.onError({
+            //    title: "stile"
+            //}))
             //.pipe($.gp.csso())
             //.pipe($.gp.sourcemaps.write())
             .pipe($.gulp.dest('build/css/'))
