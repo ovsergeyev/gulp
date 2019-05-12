09a9f8c002cdba52e603ce5d00a8095633499cb4
Sergeev Oleg <o.v.sergeyev@gmail.com>
2019-05-12 11:35:22 +0300

    В таск для работы изображений добавлена поддержка svg. Обновлен пакет cryptiles так как имеет проблемы с безопасностью

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,11 +1,11 @@
 module.exports = function () {
     $.gulp.task('img:dev', function () {
-        return $.gulp.src('src/static/img/*.{png,jpg,gif}')
+        return $.gulp.src('src/static/img/*.{png,jpg,gif,svg}')
             .pipe($.gulp.dest('build/img'));
     });
     
     $.gulp.task('img:build', function () {
-        return $.gulp.src('src/static/img/*.{png,jpg,gif}')
+        return $.gulp.src('src/static/img/*.{png,jpg,gif,svg}')
             .pipe($.gp.tinypng('78ep5ryODgDLcqnPgRzlTMiMkPuHkVPW'))
             .pipe($.gulp.dest('build/img'));
     });
